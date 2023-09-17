99260bebc6a435941fd7bf0d38a7244458a68d5d
Medeiros54 <121353502+Medeiros54@users.noreply.github.com>
2023-09-17 02:02:37 -0300

    initial commit

--- aula12.py
+++ aula12.py
@@ -1,9 +1,21 @@
 # if, elif......else
 
-entrada = input('Você quer "entrar ou "sair? ')
+'''entrada = input('Você quer "entrar ou "sair? ')
 if entrada =='entrar':
         print('Você entrou no Sistema')
 elif entrada =='sair':
             print('Você saiu do Sistema')
 else:
-        print('Você não digitou nada válido')
+        print('Você não digitou nada válido')'''
+
+
+
+condicao1 = True
+condicao2 = True
+condicao3 = True
+condicao4 = True
+
+if condicao1:
+        print('Código para Condição 1')
+        elif condicao2
+        print('Condição 2')
